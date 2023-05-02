f272b6cd931ed6779dc4315d0c21316f97b88018
Vagrant <aminebenhousniiiiiiiiiii@gmail.com>
2023-05-02 19:17:27 +0000

    Fix 5

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -6,15 +6,16 @@
  *
  * Return: numbers of nodes.
  */
+
 size_t print_listint(const listint_t *h)
 {
 	size_t nunodes = 0;
 
 	while (h != NULL)
-	{
+{
 	printf("%d\n", h->n);
 	h = h->next;
 	nunodes++;
-	}
+}
 	return (nunodes);
 }
